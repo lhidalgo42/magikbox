520fcff60bdf7b0a2cde6a475a855a75c058a69e
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2015-03-21 12:36:25 -0300

    DatabaseSeeder

---
+++ app/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+class DatabaseSeeder extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
+
+        $this->call('UsersTableSeeder');
+        $this->call('RolesTableSeeder');
+        $this->call('ProfilesTableSeeder');
+        $this->call('MacTypesTableSeeder');
+
+    }
+
+}
